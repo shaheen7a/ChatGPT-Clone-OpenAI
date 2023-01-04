73a7c01996d424956b498514bd2e75d57e363d24
Shaheen <96109270+Shaheen121@users.noreply.github.com>
2023-01-05 02:40:36 +0530

    update server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://openai-chatgpt-clone.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
